607430650aa9d9eb89ca0f4c29f14e8996e5245c
RoMaSsHoW <romasakh27@gmail.com>
2025-07-13 17:35:47 +0500

    delete[QuestionDTO]

--- FormCraft.Application/Models/DTO/QuestionDTO.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace FormCraft.Application.Models.DTO
-{
-    public class QuestionDTO
-    {
-        public string Text { get; init; } = string.Empty;
-        public string Type { get; init; } = string.Empty;
-    }
-}
