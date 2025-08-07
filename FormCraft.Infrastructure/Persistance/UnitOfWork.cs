df255c07449b6314ff62177320d2afabd1ff8d3e
RoMaSsHoW <romasakh27@gmail.com>
2025-08-07 15:04:01 +0500

    refactor[UnitOfWork]: added MassTransit

--- FormCraft.Infrastructure/Persistance/UnitOfWork.cs
+++ FormCraft.Infrastructure/Persistance/UnitOfWork.cs
@@ -1,19 +1,37 @@
 ﻿using FormCraft.Application.Common.Persistance;
+using FormCraft.Domain.Common;
+using MassTransit;
 
 namespace FormCraft.Infrastructure.Persistance
 {
     public class UnitOfWork : IUnitOfWork
     {
         private readonly FormCraftDbContext _dbContext;
+        private readonly IPublishEndpoint _publishEndpoint;
 
-        public UnitOfWork(FormCraftDbContext dbContext)
+        public UnitOfWork(
+            FormCraftDbContext dbContext, 
+            IPublishEndpoint publishEndpoint)
         {
             _dbContext = dbContext;
+            _publishEndpoint = publishEndpoint;
         }
 
         public async Task<bool> CommitAsync(CancellationToken cancellationToken = default)
         {
+            var domainEvents = _dbContext.ChangeTracker
+                .Entries<Entity>()
+                .SelectMany(e => e.Entity.DomainEvents)
+                .ToList();
+
             var result = await _dbContext.SaveChangesAsync(cancellationToken);
+
+            foreach (var entry in _dbContext.ChangeTracker.Entries<Entity>())
+                entry.Entity.ClearDomainEvents();
+
+            foreach (var domainEvent in domainEvents)
+                await _publishEndpoint.Publish(domainEvent, cancellationToken);
+
             return result > 0;
         }
     }
